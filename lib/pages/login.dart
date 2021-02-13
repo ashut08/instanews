1f7dbd734b57393fd57d372bd3c047e03521f32f
ashut08 <ashusingh.unch8@gmail.com>
2021-02-13 09:59:32 +0530

    make login and registraion in seperate pages

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instanews/pages/registration.dart';
+import 'package:instanews/services/auth_class.dart';
 //import 'package:instanews/pages/home_page.dart';
 import 'package:instanews/widgets/sigin_widget.dart';
 
@@ -11,7 +13,103 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  @override
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  //bool _autoValidate = false;
+  AuthService _authservice = AuthService();
+
+  Widget logo() {
+    return CircleAvatar(
+      backgroundColor: Colors.white,
+      radius: 48.0,
+      child: Image.asset("assets/images/login.png"),
+
+      /* Text("Sign In page") */
+    );
+  }
+
+  _emailtextField() {
+    return TextFormField(
+      autocorrect: true,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      //initialValue: 'alucard@gmail.com',
+      decoration: InputDecoration(
+          hintText: "Enter email",
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      autocorrect: true,
+      controller: passwordController,
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      obscureText: false,
+      //initialValue: 'alucard@gmail.com',
+      decoration: InputDecoration(
+          hintText: "Enter password",
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+  }
+
+  Widget signinButton() {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24),
+      ),
+      onPressed: () async {
+        // widget.toogleview();
+        /*  dynamic result = await _authservice.signInAnom();
+          if (result == null) {
+            print("error in signin");
+          } else {
+            print("signin");
+            print(result);
+          } */
+        //Navigator.push(
+        //context, MaterialPageRoute(builder: (_) => HomePage()));
+      },
+      padding: EdgeInsets.all(12),
+      color: Colors.lightBlueAccent,
+      child: Text('Log In', style: TextStyle(color: Colors.white)),
+    );
+  }
+
+  Widget forgotButton() {
+    return FlatButton(
+      child: Text(
+        'Forgot password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget newUserSignUp() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("New to InstaNews"),
+        FlatButton(
+          child: Text("Register Here"),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => RegistrationPage()));
+          },
+        )
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -26,7 +124,35 @@ class _LoginPageState extends State<LoginPage> {
               fontFamily: 'Raleway'),
         ),
       ),
-      body: SignInWidget(),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          // ignore: deprecated_member_use
+
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(children: <Widget>[
+                logo(),
+                SizedBox(
+                  height: 30,
+                ),
+                _emailtextField(),
+                SizedBox(
+                  height: 8,
+                ),
+                passwordField(),
+                SizedBox(height: 24.0),
+                signinButton(),
+                SizedBox(
+                  height: 20,
+                ),
+                forgotButton(),
+                newUserSignUp(),
+              ]),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/registration.dart
+++ lib/pages/registration.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:instanews/widgets/registration_widget.dart';
+import 'package:instanews/pages/login.dart';
+import 'package:instanews/services/auth_class.dart';
 
 class RegistrationPage extends StatefulWidget {
   final Function toggleview;
@@ -10,8 +11,83 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
-  @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    AuthService _authservice = AuthService();
+    _emailtextField() {
+      return TextFormField(
+        autocorrect: true,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        //initialValue: 'alucard@gmail.com',
+        decoration: InputDecoration(
+            hintText: "Enter email",
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      );
+    }
+
+    Widget passwordField() {
+      return TextFormField(
+        autocorrect: true,
+        controller: passwordController,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        obscureText: false,
+        //initialValue: 'alucard@gmail.com',
+        decoration: InputDecoration(
+            hintText: "Enter password",
+            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      );
+    }
+
+    Widget signupButton() {
+      return MaterialButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () async {
+          // widget.toogleview();
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.lightBlueAccent,
+        child: Text('SignUp', style: TextStyle(color: Colors.white)),
+      );
+    }
+
+    Widget existingUserSignIn() {
+      return Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Already have a account"),
+          FlatButton(
+            child: Text("Login"),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => LoginPage()));
+            },
+          )
+        ],
+      );
+    }
+
+    Widget logo() {
+      return CircleAvatar(
+        backgroundColor: Colors.black,
+        radius: 48.0,
+        //child: Image.asset("assets/images/logo.jpg"),
+
+        /* Text("Sign In page") */
+      );
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -25,7 +101,34 @@ class _RegistrationPageState extends State<RegistrationPage> {
               fontFamily: 'Raleway'),
         ),
       ),
-      body: SignUp(),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          // ignore: deprecated_member_use
+
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(children: <Widget>[
+                logo(),
+                SizedBox(
+                  height: 30,
+                ),
+                _emailtextField(),
+                SizedBox(
+                  height: 8,
+                ),
+                passwordField(),
+                SizedBox(height: 24.0),
+                signupButton(),
+                SizedBox(
+                  height: 25,
+                ),
+                existingUserSignIn(),
+              ]),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
